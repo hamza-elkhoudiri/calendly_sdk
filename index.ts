250da498c978e402f7c94a4ea1ed2ac51f49574e
Hamza Elkhoudiri <hamza.elkhoudiri@kustomer.com>
2022-07-11 10:32:33 -0400

    Added kview

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
   throw new Error("clientId and clientSecret are required");
 }
 
-const APP_VERSION = "3.0.29";
+const APP_VERSION = "3.0.30";
 
 const options: AppOptions = {
   app: "calendly_sdk",
