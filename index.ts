6bedf676fd4c10e692a7abcace760e5e07e52789
Hamza Elkhoudiri <hamza.elkhoudiri@kustomer.com>
2022-07-12 14:54:06 -0400

    changing version

--- index.ts
+++ index.ts
@@ -15,7 +15,7 @@ if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET) {
   throw new Error("clientId and clientSecret are required");
 }
 
-const APP_VERSION = "3.0.42";
+const APP_VERSION = "3.0.43";
 
 const options: AppOptions = {
   app: "calendly_sdk",
